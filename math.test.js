fbcb3717bf6170f55be1fb4cceea7c8f44e620b5
mahdimortuza <mahdi.webx@gmail.com>
2024-06-15 22:14:02 +0600

    first test

---
+++ math.test.js
@@ -0,0 +1,8 @@
+import { expect, it } from 'vitest'
+import { add } from './math'
+
+it('should return the correct sum if an array of number is provided', () =>{
+    const result = add([1,2,3])
+
+    expect(result).toBe(6)
+}) 
