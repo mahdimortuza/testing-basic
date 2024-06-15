260e8a0aa7b42239cc6e1b7450f80157c1e1569a
mahdimortuza <mahdi.webx@gmail.com>
2024-06-15 23:26:19 +0600

    with no arguments

--- math.js
+++ math.js
@@ -1,7 +1,7 @@
 export function add(numbers) {
     let sum = 0;
     for (const number of numbers) { // Corrected the syntax here
-        sum += number;
+        sum += +number;
     }
     return sum;
 }
--- math.test.js
+++ math.test.js
@@ -1,6 +1,15 @@
 import { expect, it } from 'vitest'
 import { add } from './math'
 
+
+// AAA pattern (better way to write test)
+// a === arrange
+// a === actions
+// a === assertion
+
+// ------------------------------ video 1-3 ----------------------------------------
+
+// test case - 1
 it('should return the correct sum if an array of number is provided', () =>{
 
     // arrange
@@ -14,7 +23,68 @@ it('should return the correct sum if an array of number is provided', () =>{
     expect(result).toBe(expectedResult)
 }) 
 
-// AAA pattern (better way to write test)
-// a === arrange
-// a === actions
-// a === assertion
+
+// test case - 2
+it('should provide NaN if at least one invalid number is provided', () =>{
+
+    // arrange
+    const numbers = [1,'invalid',3] 
+
+    // actions
+    const result = add(numbers)
+
+    // assertion
+    expect(result).toBeNaN()
+}) 
+
+
+
+// test case - 3
+it('should provide correct sum if an array numeric string is provided', () =>{
+
+    // arrange
+    const numbers = ['1','3','3']
+    const expectedResult = numbers.reduce((acc, cur) => +acc + +cur)
+
+    // actions
+    const result = add(numbers)
+
+    // assertion
+    expect(result).toBe(expectedResult)
+}) 
+
+
+
+// ------------------------------ video 4 ----------------------------------------
+
+// test case - 4  (throwing error)
+it('should throw an error if no argument is passed', () =>{
+
+    try {
+        const result = add()
+    
+    } catch (error) {
+    expect(error).toBeDefined()
+    }
+
+}) 
+
+
+
+
+// better way
+it('should throw an error if no argument is passed', () =>{
+    const resultFn = () => {
+        add()
+    }
+     expect(resultFn).toThrow()
+}) 
+
+
+// multiple arguments
+it('should throw an error if multiple argument is provided', () =>{
+    const resultFn = () => {
+        add(1,2,3),
+    }, 
+    expect(resultFn).toThrow()
+}) 
