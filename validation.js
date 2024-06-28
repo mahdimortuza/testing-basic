1e6aeff5c96e332137a4b88d3f530650f88a19b3
mahdimortuza <mahdi.webx@gmail.com>
2024-06-28 14:36:03 +0600

    validating email field

---
+++ validation.js
@@ -0,0 +1,13 @@
+export function validateEmail(email){
+    const emailRegex = /\s+@\s+\.\s+/;
+
+    if(!emailRegex.test(email)){
+        throw new Error ("Invalid email address")
+    }
+}
+
+export function validateArrayNotEmpty(arr) {
+    if(!Array.isArray(arr) || arr.length === 0){
+        throw new Error('Array can not be empty')
+    }
+}
